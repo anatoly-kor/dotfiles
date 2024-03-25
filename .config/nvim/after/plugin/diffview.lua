b34939c10042f62dcfa3a017f78d2eda5b30c446
Anatoly Korobov <a.korobov@vk.team>
2024-03-25 08:59:43 +0300

    add desc to couple keys

--- .config/nvim/after/plugin/diffview.lua
+++ .config/nvim/after/plugin/diffview.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n", "<leader>df", ":DiffviewFileHistory %<CR>")
-vim.keymap.set("n", "<leader>dc", ":DiffviewClose<CR>")
+vim.keymap.set("n", "<leader>df", ":DiffviewFileHistory %<CR>", { desc = "[D]iffview [F]ile History" })
+vim.keymap.set("n", "<leader>dc", ":DiffviewClose<CR>", { desc = "[D]iffview [C]lose" })
