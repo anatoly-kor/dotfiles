d5e493c2543fb4c3997d5846d694c7e739c331a0
Anatoly Korobov <ak@MacBook-Pro-Anatoly.local>
2024-05-16 12:17:47 +0300

    update hjkl plugin for express_line

--- .config/nvim/lua/ak/plugins/statusline.lua
+++ .config/nvim/lua/ak/plugins/statusline.lua
@@ -34,15 +34,15 @@ return {
             return extensions.git_changes(window, buffer)
         end)
 
-        local hjkl_output = function()
+        local hjkl_output = function(_, _)
             local hp_list = require("harpoon.mark")
             local total_marks = hp_list.get_length()
+
             if total_marks == 0 then
-                return ""
+                return "hjkl"
             end
 
             local current_buf_index = hp_list.get_current_index()
-
             local output = {}
 
             for i, keymap in ipairs({ "h", "j", "k", "l" }) do
@@ -65,7 +65,7 @@ return {
 
             table.insert(el_segments, " ")
 
-            table.insert(el_segments, hjkl_output())
+            table.insert(el_segments, subscribe.buf_autocmd("hjkl", "BufEnter", hjkl_output))
 
             table.insert(el_segments, sections.split)
 
